2467e4be269480d2be25c37198c612bdb09cf1a3
LeastKIds <leastkids@gmail.com>
2022-11-08 09:23:49 +0900

    HelloApp 完成

---
+++ 0004_HelloApp/0004_HelloApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  0004_HelloApp
+//
+//  Created by 김진홍 on 2022/11/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+
+        Text("こんにちは")
+            .font(.title)
+            .foregroundColor(.blue)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 0004_HelloApp/0004_HelloApp/_004_HelloAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _004_HelloAppApp.swift
+//  0004_HelloApp
+//
+//  Created by 김진홍 on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct _004_HelloAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
