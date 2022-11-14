d5008c00bd8a0cd7af6ca58be452331e380b60ee
LeastKIds <leastkids@gmail.com>
2022-11-14 10:38:55 +0900

    CardApp animation

--- 0011_CardApp/0011_CardApp/ContentView.swift
+++ 0011_CardApp/0011_CardApp/ContentView.swift
@@ -18,22 +18,54 @@ struct ContentView: View {
     @State var isJapanes = true
     
     var body: some View {
-        VStack {
-            Text(isJapanes ? japanes : cards[japanes]!)
+        ZStack {
+            Color(red: 0.97, green: 0.96, blue: 0.56)
+                .edgesIgnoringSafeArea(.all)
             
-            HStack {
-                Button(action: {self.isJapanes.toggle()}){
-                    Text("裏返す")
-                } //Button
+            VStack {
                 
-                Button(action: {
-                    self.isJapanes = true
-                    self.japanes = self.cards.randomElement()!.key
-                }){
-                    Text("次へ")
-                } // Button
-            } // HStack
-        } // VStack
+                ZStack {
+                    Image("card")
+                        .resizable()
+                        .frame(width: 300.0, height: 133.0)
+                        .shadow(radius: 3)
+                        .rotation3DEffect(.degrees(isJapanes ? 0 : 180), axis: (x: 0, y: 1, z: 0))
+                        .animation(.spring())
+                        .onTapGesture {
+                            self.isJapanes.toggle()
+                        }
+                    
+                    Text(isJapanes ? japanes : cards[japanes]!)
+                        .font(.largeTitle)
+                    
+                }   // ZStack
+                .padding()
+                
+                HStack {
+                    Group {
+                        Button(action: {self.isJapanes.toggle()}){
+                            Image(systemName: "arrow.2.circlepath")
+                            Text("裏返す")
+                        } //Button
+                        
+                        Button(action: {
+                            self.isJapanes = true
+                            self.japanes = self.cards.randomElement()!.key
+                        }){
+                            Image(systemName: "forward.fill")
+                            Text("次へ")
+                        } // Button
+                    }   // Gruop
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color(red: 0.86, green: 0.45, blue: 0.03))
+                    .cornerRadius(10)
+                    
+                } // HStack
+            } // VStack
+            
+            
+        }   // ZStack
     }
 }
 
