e3b0344349e39871b16ac2fa978ca5e0c8656995
LeastKIds <leastkids@gmail.com>
2022-11-21 14:46:31 +0900

    LaunchScreen

---
+++ 0013_LaunchScreen/0013_LaunchScreen/_013_LaunchScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  _013_LaunchScreenApp.swift
+//  0013_LaunchScreen
+//
+//  Created by 김진홍 on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct _013_LaunchScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
